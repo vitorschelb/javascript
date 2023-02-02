b1590864ba3f0da1e03f30db6601172ecf0a06c3
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-02-02 13:02:46 -0300

    Bitwise

---
+++ cbf-curso/aula08-operadoresbitwise.js
@@ -0,0 +1,14 @@
+let n1=10
+let n2=11
+let res = n1 & n2
+console.log(res)
+
+
+// Ele retorna 10 pois tem equivalência binária.
+// 10 = 1010 11 = 1011
+
+// 10 = 00001010
+// 11 = 00001011
+
+//      00001010 { Semelhante ao &&, 1+1=1 0+0=0 1+1=1 0+1=0}
+
