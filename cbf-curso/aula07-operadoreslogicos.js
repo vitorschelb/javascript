b2cedc7b63189d690ff1b6542b7e51894d93808a
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-02-02 12:44:22 -0300

    Operadores Lógicos

---
+++ cbf-curso/aula07-operadoreslogicos.js
@@ -0,0 +1,28 @@
+// && -> and e 
+// || -> or ou 
+// ! -> not não
+
+//Tabela Verdade
+
+// && 
+
+// V + V = V
+// V + F = F
+// F + V = F
+// F + F = F 
+
+// ||
+
+// V + V = V
+// V + F = V
+// F + V = V
+// F + F = F
+
+let n1,n2,n3,n4
+n1=10 
+n2=5 
+n3=15 
+n4=2
+
+console.log((n1>n2)&&(n1>n3))
+
