76b6ba27d7748dbe3bbc6342440c3a01058ee4aa
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-02-08 18:48:30 -0300

    this

--- cbf-curso/aula29-operadorthisfuncao.js
+++ cbf-curso/aula29-operadorthisfuncao.js
@@ -1,1 +1,12 @@
-//O This indica que a variavel indica para um parametro especifico dentro da função
+//O This indica que a variavel indica para um parametro especifico dentro da função. Faz referência ao elemento DENTRO DA FUNÇÃO;
+
+function aluno(nome,nota){
+    this.nome=nome
+    this.nota=nota
+
+    console.log(nome)
+    console.log(nota)
+}
+
+aluno("Bruno", 100)
+
