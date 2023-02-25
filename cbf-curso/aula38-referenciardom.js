bd3325685d49dc0720454ad907b03f1c0651c390
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-02-24 21:07:37 -0300

    DOM

---
+++ cbf-curso/aula38-referenciardom.js
@@ -0,0 +1,7 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c=[...document.querySelectorAll(".curso")]
+
+console.log(caixa1.firstElementChild)
+console.log(caixa1.lastElementChild)
+console.log(caixa1.children)
+console.log(document.getRootNode()) //METODO DIFERENTE DE PROPRIEDADE, SEMPRE É ACOMPANHADO DE PARENTESES
