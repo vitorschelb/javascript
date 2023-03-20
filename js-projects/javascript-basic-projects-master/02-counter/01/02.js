4ee2959f678722b86d07558d2be854b15760a817
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-03-20 19:32:44 -0300

    projects

--- js-projects/javascript-basic-projects-master/02-counter/01/02.js
+++ js-projects/javascript-basic-projects-master/02-counter/01/02.js
@@ -1,6 +1,8 @@
-// set inital value to zero
+// set initial count
 let count = 0;
-// select value and buttons
+
+//select value and buttons
+
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
@@ -14,15 +16,11 @@ btns.forEach(function (btn) {
     } else {
       count = 0;
     }
-
     if (count > 0) {
       value.style.color = "green";
     }
     if (count < 0) {
       value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
     }
     value.textContent = count;
   });
---
+++ js-projects/javascript-basic-projects-master/03-reviews/03.js
@@ -0,0 +1,93 @@
+//--- Reviews Data ---
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+//--- Select Elements ---
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+//--- Set starting item ---
+let currentItem = 0;
+
+//--- Load initial item ---
+
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+//Show person based on item
+const showPerson = () => {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+};
+
+//Show person based on random
+// const showRandom = () => {
+//   const random = Math.floor(Math.random() * reviews.length);
+//   const item = reviews[random];
+//   img.src = item.img;
+//   author.textContent = item.name;
+//   job.textContent = item.job;
+//   info.textContent = item.text;
+// };
+
+//Show next person
+nextBtn.addEventListener("click", () => {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+//Show previous person
+prevBtn.addEventListener("click", () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+//Show random person
+randomBtn.addEventListener("click", () => {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
