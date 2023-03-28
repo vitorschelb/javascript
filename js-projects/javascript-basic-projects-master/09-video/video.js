988e9c2cb7a75fb8d7868780d4b81adc838c9b4a
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-03-28 20:16:08 -0300

    Scroll

---
+++ js-projects/javascript-basic-projects-master/09-video/video.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+const preloader = document.querySelector(".preloader")
+
+btn.addEventListener("click", () => {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.toggle("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
+
+window.addEventListener("load", () => {
+    preloader.classList.add("hide-preloader");
+})
+
